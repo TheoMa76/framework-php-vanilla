655efb4dc6ee62fb1104f3067b0568b75f38776c
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-10-06 15:48:08 +0200

    index update

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
 require_once './configs/bootstrap.php';
+ob_start();
 if(isset($_GET["page"])){
     fromInc($_GET['page']);
 }
-
+$contacts = $connection->query(queryBuilder('r', 'contacts'));
 $pageContent = [
-    'html' => ob_get_clean()
+    "html" => ob_get_clean(),
+    "data" => [
+        'contacts' => $contacts
+    ]
 ];
-include "./templates/layout/". $_GET["layout"] .".layout.php";
-getAll($connection);
-getById($connection,1);
 
+include "./templates/layouts/". $_GET["layout"] .".layout.php";
 

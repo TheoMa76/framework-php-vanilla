57d0d4c5fa7dcaffbf380032fe566cf9f58ea048
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-10-06 11:21:47 +0200

    crud

--- src/crud.php
+++ src/crud.php
@@ -1,32 +1,28 @@
-<?php
+<?php 
 require_once "./src/dbConnect.php";
 
 
 
-function getById(){
-    $connection->query("SELECT * FROM contacts WHERE id = '".htmlspecialchars($_GET['id']));
-    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
-}
+//fonction getAll
+$statement = $connection->query("SELECT * FROM contacts WHERE 1");
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+//fonction getById
+$statement = $connection->query("SELECT * FROM contacts WHERE `name` =  'Delaistre' AND `surname` = '".htmlspecialchars( $_GET["surname"])."'");
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+//fonction create 
+$statement = $connection->prepare("INSERT INTO `contacts` (`name`, `surname`, `status`) VALUES (?, ?, 'online') ");
+$statement->bindParam(1,$_GET["name"]);
+$statement->bindParam(2,$_GET["surname"]);
+$statement->execute();
 
-function create(){
-    $statement = $connection->prepare("INSERT INTO `contacts` (`name`, `surname` , `status`) VALUES ('Maerten' , 'Theo' , 'online') ");
-    //$statement->bindParam(1,$_GET['surname']);
-    $statement->execute();
-}
+//fonction delete
+$statement = $connection->prepare("DELETE FROM `contacts` WHERE id = ?");
+$id = 3;
+$statement->bindParam(1, $id);
+$statement->execute();
+//fonction update
 
-function getAll(){
-    $statement = $connection->query("SELECT * FROM `contacts` WHERE 1");
-    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function delete(){
-
-}
-
-function deleteById(){
-
-}
-
-function update(){
-    
-}
+$statement = $connection->prepare("UPDATE `contacts` SET `status` = 'offline' WHERE id = ?");
+$id = 2;
+$statement->bindParam(1, $id);
+$statement->execute();

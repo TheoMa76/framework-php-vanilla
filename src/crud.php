034f68f38d178980dead50ff6b4405f6537b3863
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-10-06 14:25:21 +0200

    fix

--- src/crud.php
+++ src/crud.php
@@ -7,28 +7,32 @@ function getAll($connection){
     $statement = $connection->query("SELECT * FROM `contacts` WHERE 1");
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
     dd($data);
+    return $data;
 }
 function getById($connection, $id){
     $statement = $connection->prepare("SELECT * FROM `contacts` WHERE id =  ?");
     $statement->bindParam(1,$id);
     $statement->execute();
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
-
+    return $data;
 }
 function create($connection,$name,$surname){ 
     $statement = $connection->prepare("INSERT INTO `contacts` (`name`, `surname`, `status`) VALUES (?, ?, 'online') ");
     $statement->bindParam(1,$name);
     $statement->bindParam(2,$surname);
     $statement->execute();
+    return $data;
 }
 function deleteById($connection, $id){
     $statement = $connection->prepare("DELETE FROM `contacts` WHERE id = ?");
     $statement->bindParam(1, $id);
     $statement->execute();
+    return $data;
 }
 function update($connection, $column,$valeur,$id){
     $statement = $connection->prepare("UPDATE `contacts` SET `".$column."` = ? WHERE id = ? ");
     $statement->bindParam(2, $id);
     $statement->bindParam(1, $valeur);
     $statement->execute();
+    return $data;
 }

bc3fa75fa36a0a2aced2aab8f85a5e0ce8891259
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-10-27 14:23:28 +0200

    bug

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -4,6 +4,7 @@ use Theo\Controller\Database;
 
 class QueryTest extends TestCase{
 
+    //test phpunit pour tester les methode
     public function testMethod(){
         $database = new Database();
         $this->assertEquals("get", $database->get([])->getMethod());
@@ -13,6 +14,7 @@ class QueryTest extends TestCase{
         $this->assertEquals('delete', $database->delete([],true)->getMethod());
     }
 
+    //test phpunit pour tester format
     public function testFormat(){
         $database = new Database();
         $this->assertEquals("SELECT %s FROM %s WHERE %s ;", $database->get([])->getFormat());
@@ -20,5 +22,11 @@ class QueryTest extends TestCase{
         $this->assertEquals("UPDATE %s SET %s WHERE %s ;", $database->delete([])->getFormat());
         $this->assertEquals("DELETE FROM %s WHERE %s ;", $database->delete([],true)->getFormat());
         $this->assertEquals("INSERT INTO %s %s VALUES %s ;",$database->post([])->getFormat());
+    }
+
+    //test phpunit pour tester les params
+    public function testParams(){
+        $database = new Database();
+        $this->assertEquals('name = "Maerten" AND surname = "Theo"', $database->get(['filters'=>['name'=>'Maerten','surname'=>'Theo']])->parseParams());        
     }
 }
